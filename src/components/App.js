c32c459c26cb7afcb5ef3f67b20130efae21070a
malknaor <malknaor@gmail.com>
2019-09-25 09:36:19 +0300

    first commit with App and Header components

---
+++ src/components/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+import Header from './Header';
+import '../css/App.css';
+
+const App = () => {
+    return  (
+        <div className="ui container app">
+            <Header />
+        </div>
+    );
+};
+
+export default App;
